0035c4458ad1ecef92941572fce9b2a12eea42d8
Ziphil <yfe23457@nifty.com>
2024-04-24 12:51:37 +0900

    変更

--- renderer/component/core/routing/routing.tsx
+++ renderer/component/core/routing/routing.tsx
@@ -1,5 +1,7 @@
-//
+/* eslint-disable react/jsx-closing-bracket-location */
 
+import {faCircleNotch} from "@fortawesome/pro-regular-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {ReactElement, Suspense} from "react";
 import {RouterProvider, createHashRouter} from "react-router-dom";
 import {create} from "/renderer/component/create";
@@ -23,7 +25,11 @@ export const Routing = create(
     useAutoUpdateToday();
 
     return (
-      <Suspense fallback={<div/>}>
+      <Suspense fallback={(
+        <div styleName="loading">
+          <FontAwesomeIcon styleName="icon" icon={faCircleNotch} spin={true}/>
+        </div>
+      )}>
         <RouterProvider router={router}/>
       </Suspense>
     );
